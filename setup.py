795263b07ae22dc50a2484d92494c90bc56e3729
Andrew Martin <a.a.d.martin@gmail.com>
2016-08-15 23:31:02 +1000

    Added deps

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-install_requires = [
-    'bamboo_api'
-    ]
 
 setup(
     name="pipectl",
-    version="0.1.2",
+    version="0.1.7",
     description="A CI/CD Build pipeline DSL",
     license="MIT",
     author="adamar",
     author_email="none@none.com",
     url="http://github.com/adamar/pipectl",
     packages=find_packages(),
-    dependency_links=['http://github.com/adamar/python-bamboo-api/tarball/master#egg=bamboo_api'],
+    install_requires=[
+        "bamboo_api",
+        "GitPython>=2.0.0"
+        ],
+    dependency_links=[
+        'git://github.com/adamar/python-bamboo-api.git#egg=bamboo_api'
+        ],
     scripts=['pipectl/pipectl'],
     classifiers=[
         "Programming Language :: Python",
