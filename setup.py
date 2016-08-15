6395aced7ae8bbeb50c2adab4f858d15588717cf
Andrew Martin <a.a.d.martin@gmail.com>
2016-08-15 14:49:58 +1000

    Renaming

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ install_requires = [
 ]
 
 setup(
-    name="cushing",
+    name="pipectl",
     version="0.1.0",
     description="A CI/CD Build pipeline DSL",
     license="MIT",
     author="adamar",
     author_email="none@none.com",
-    url="http://github.com/adamar/cushing",
+    url="http://github.com/adamar/pipectl",
     packages=find_packages(),
     install_requires=install_requires,
-    scripts=['cushing/cushing'],
+    scripts=['pipectl/pipectl'],
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
