9f340a577724b38a59804a8b844ce1ce9a7083d7
Andrew Martin <a.a.d.martin@gmail.com>
2016-08-18 15:58:38 +1000

    slight fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pipectl",
-    version="0.1.20",
+    version="0.1.22",
     description="A CI/CD Build pipeline DSL",
     license="MIT",
     author="adamar",
