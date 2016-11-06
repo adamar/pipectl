07312a190cc4ff90e5d23c45f3fbd349a6538c3a
Andrew Martin <a.a.d.martin@gmail.com>
2016-11-07 10:47:24 +1100

    bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pipectl",
-    version="0.1.25",
+    version="0.1.26",
     description="A CI/CD Build pipeline DSL",
     license="MIT",
     author="adamar",
